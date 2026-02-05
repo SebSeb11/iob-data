1445f3f93da286f044e72eeb2fcf01fc2557e73b
SebSeb11 <48764817+SebSeb11@users.noreply.github.com>
2026-02-05 18:23:00 +0100

    Update temperature.js via Node-RED

--- temperature.js
+++ temperature.js
@@ -4,10 +4,10 @@ document.getElementById("temperature").innerHTML = `
        alt="Temp"
        style="width:20px; height:20px; object-fit:contain;">
   <span style="font-size: 1rem; font-weight: bold;">
-    14.6 °C
+    13.9 °C
   </span>
   <span style="font-size: 0.5rem;">
-    (05.02.26, 17:55)
+    (05.02.26, 18:15)
   </span>
 </div>
 `;
